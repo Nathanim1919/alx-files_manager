a03ad5fde0f3f6a5fa94d86b5ccb7ff78f9a7872
Nathanim1919 <nathanim2tadele@gmail.com>
2024-06-19 11:09:42 +0300

    db util implementation

--- utils/db.js
+++ utils/db.js
@@ -1,38 +1,38 @@
-import mongodb from 'mongodb';
+import mongodb from "mongodb";
+
+
+const port = process.env.DB_PORT || 27017;
+const host = process.env.DB_HOST || "localhost";
+const uri = `mongodb://${host}:${port}`;
+const dbName = process.env.DB_DATABASE || "file_manager";
 
 class DBClient {
   constructor() {
-    const uri = `mongodb://${process.env.DB_HOST || 'localhost'}:${
-      process.env.DB_PORT || 27017
-    }`;
-    const dbName = process.env.DB_DATABASE || 'file_manager';
-
     this.client = new mongodb.MongoClient(uri, { useUnifiedTopology: true });
-
     this.database = null;
     this.client
       .connect()
       .then((client) => {
         this.database = client.db(dbName);
-        this.database.createCollection('users');
-        this.database.createCollection('files');
+        this.database.createCollection("users");
+        this.database.createCollection("files");
       })
       .catch((err) => {
-        console.log('Failed to connect to MongoDB ', err.message);
+        console.log("Failed to connect to MongoDB ", err.message);
       });
   }
 
   isAlive() {
-    return this.client.isConnected();
+    return !!this.database;
   }
 
   async nbUsers() {
-    const usersCollection = this.database.collection('users');
+    const usersCollection = this.database.collection("users");
     return usersCollection.countDocuments();
   }
 
   async nbFiles() {
-    const filesCollection = this.database.collection('files');
+    const filesCollection = this.database.collection("files");
     return filesCollection.countDocuments();
   }
 }
